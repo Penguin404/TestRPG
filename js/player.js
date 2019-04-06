ab1bee72594844465bf8578a161edb2f6a7ad76d
Penguin404 <macpenguin0@gmail.com>
2019-04-05 23:15:21 -0400

    Made code easier to handle and added instructions

---
+++ js/player.js
@@ -0,0 +1,118 @@
+import Scene from "./scene.js"
+
+
+export default class Player {
+    constructor(scene, x, y){
+        
+        this.scene = scene;
+        this.anims = scene.anims;
+        this.sprite = scene.physics.add.sprite(x, y, 'player', 0)
+        this.sprite.setCollideWorldBounds(true);
+        
+        const { LEFT, RIGHT, UP, DOWN, W, A, S, D, X, SPACEBAR } = Phaser.Input.Keyboard.KeyCodes;
+    this.keys = scene.input.keyboard.addKeys({
+      left: LEFT,
+      right: RIGHT,
+      up: UP,
+      down: DOWN,
+      w: W,
+      a: A,
+      s: S,
+      d: D,
+      spacebar:SPACEBAR,
+      x: X,
+
+    });
+        //  animation with key 'left', we don't need left and right as we will use one and flip the sprite
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('player', { frames: [1, 5, 1, 9]}),
+        frameRate: 10,
+        repeat: -1
+    });
+    // animation with key 'right'
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('player', { frames: [1, 5, 1, 9] }),
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'up',
+        frames: this.anims.generateFrameNumbers('player', { frames: [2, 6, 2, 10]}),
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'down',
+        frames: this.anims.generateFrameNumbers('player', { frames: [ 0, 4, 0, 8 ] }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'exist',
+        frames: this.anims.generateFrameNumbers('fireball', { frames: [ 0, 1 ] }),
+        frameRate: 10,
+        repeat: -1
+    });
+    }
+    
+    update(time, delta)
+    {
+        const keys = this.keys;
+        this.sprite.body.setVelocity(0);
+        
+        // Horizontal movement
+        if (keys.left.isDown)
+        {
+            this.sprite.body.setVelocityX(-80);
+            
+        }
+        else if (keys.right.isDown)
+        {
+            this.sprite.body.setVelocityX(80);
+        } 
+        this.sprite.body.velocity.normalize().scale(80);
+ 
+        // Vertical movement
+        if (keys.up.isDown)
+        {
+            this.sprite.body.setVelocityY(-80);
+        }
+        else if (keys.down.isDown)
+        {
+            this.sprite.body.setVelocityY(80);
+            
+        }    
+        
+
+        if (keys.spacebar.isDown || Phaser.Input.Keyboard.JustDown(keys.x))
+        {
+            this.scene.addFireball()
+        }
+        //animation
+        if (keys.left.isDown)
+        {
+            this.sprite.anims.play('left', true);
+            this.sprite.flipX = true;
+        }
+        else if (keys.right.isDown)
+        {
+            this.sprite.anims.play('right', true);
+            this.sprite.flipX = false;
+        }
+        else if (keys.up.isDown)
+        {
+            this.sprite.anims.play('up', true);
+        }
+        else if (keys.down.isDown)
+        {
+            this.sprite.anims.play('down', true);
+        }
+        else
+        {
+            this.sprite.anims.stop();
+        }
+    }
+}
---
+++ js/scene.js
@@ -0,0 +1,85 @@
+import Player from "./player.js"
+
+export default class Scene extends Phaser.Scene {
+    
+    preload()
+    {
+        // map tiles
+        this.load.image('tiles', 'assets/map/RPGTileset.png');
+        
+        // map in json format
+        this.load.tilemapTiledJSON('map', 'assets/map/map.json');
+        
+        // our two characters
+        this.load.spritesheet('player', 'assets/RPG_assets.png', { frameWidth: 16, frameHeight: 16 });
+
+        this.load.spritesheet('enemy', 'assets/RPG_assets.png', { frameWidth: 16, frameHeight: 16 });
+        this.load.spritesheet('fireball', 'assets/fireball.png', { frameWidth: 16, frameHeight: 16 });
+
+    }
+ 
+    create()
+    {
+        var map = this.make.tilemap({ key: 'map' });
+        
+        var tiles = map.addTilesetImage('RPGTileset', 'tiles');
+        
+	var grass = map.createStaticLayer('Background', tiles, 0, 0);
+        var obstacles = map.createStaticLayer('Blocked', tiles, 0, 0);
+        obstacles.setCollisionByExclusion([-1]);
+        
+        this.player = new Player(this,50,50);
+        
+
+        this.physics.world.bounds.width = map.widthInPixels;
+        this.physics.world.bounds.height = map.heightInPixels;
+        
+
+        
+
+        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+        this.cameras.main.startFollow(this.player.sprite);
+        this.cameras.main.roundPixels = true;
+
+        this.physics.add.collider(this.player.sprite, obstacles);
+                         
+        this.physics.add.overlap(this.player, this.enemy, this.onMeetEnemy, false, this);
+
+        this.add.text(250, 1, "Arrow keys\n to move,\n x key \nto shoot \nfireballs.", {
+        font: "10px monospace",
+        fill: "#000000",
+        padding: { x: 5, y: 5 },
+        backgroundColor: "#ffffff"
+        
+      })
+      .setScrollFactor(0);
+
+      
+  }
+
+         
+
+    addFireball()
+    {
+        this.fireball = this.physics.add.sprite(this.player.sprite.x, this.player.sprite.y, 'fireball', 1);
+        this.fireball.setVelocityY(-50);
+        
+    }
+    
+
+    onMeetEnemy(player, zone) 
+    {        
+        // we move the zone to some other location
+              
+ 
+        // start battle 
+    }
+
+    update(time, delta)
+    {
+        
+        this.player.update();
+        
+        
+    }
+}
